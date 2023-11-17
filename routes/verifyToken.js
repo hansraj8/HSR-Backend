33d68c14a728c719c68b4f980b389907b2aa050f
Hansraj <hansrajsharma8252@gmail.com>
2023-11-18 02:33:32 +0530

    error fixed

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
-    const authHeader = req.headers['auth-token']; // Updated to use 'auth-token'
+    const authHeader = req.headers.token; // Updated to use 'auth-token'
     if (authHeader) {
         const token = authHeader.split(" ")[1];
         jwt.verify(token, process.env.JWT_SEC, (err, user) => {
